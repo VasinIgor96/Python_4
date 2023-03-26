a2b11c284840eba390633d3281b7838bdff3ccbb
Vasin Igor <vasin.igor0796@gmail.net>
2023-03-26 13:22:27 +0300

    ex_1 Task done

--- Python_4.py
+++ Python_4.py
@@ -1,1 +1,9 @@
 
+begin = int(input("Введіть початкове число діапазону: "))
+end = int(input("Введіть кінцеве число діапазону: "))
+
+if begin > end:
+    begin, end == end, begin
+for i in range (begin, end +1):
+    if i %7 == 0:
+       print(i)
