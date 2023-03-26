072f038ed70d305dafaaf45392ca9822f94a508a
student <student@PC-G-04>
2023-03-26 11:51:39 +0300

    Task

--- Python_4.py
+++ Python_4.py
@@ -1,1 +1,17 @@
+s=13
+e=33
+begin = end =-1
+begin = int(input('Enter a begin: '))
+end = int(input('Enter an end: '))
 
+if begin > end:
+    begin, end = end, begin
+else:
+    begin, end = end, begin
+while begin < s or begin > e:
+      begin = int(input("Enter"))
+while end < s or end > e:
+      end = int(input("Enter"))
+for i in range(begin, end +1):
+    if i %2 !=0:
+       print(i)
