cbef2301d6a5f6582cc030d23ff4d50800129f7f
Vasin Igor <vasin.igor0796@gmail.net>
2023-03-26 15:34:33 +0300

    Ex_3 Task done

--- Python_4.py
+++ Python_4.py
@@ -1,1 +1,13 @@
 
+begin = int(input("Введіть початок діапазону: "))
+end = int(input("Введіть кінець діапазону: "))
+
+for i in range(begin, end+1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("Це число кратне 3 та 5","'Fizz Buzz'")
+    elif i % 3 == 0:
+        print("Це число кратне 3","'Fizz'")
+    elif i % 5 == 0:
+        print("Це число кратне 5","'Buzz'")
+    else:
+        print("Це число не кратне ні 3, ні 5: ",i)
